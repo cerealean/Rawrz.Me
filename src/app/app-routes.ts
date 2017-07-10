89212e9c72c3d97da078fce5fa7381acc5682231
Michael.Crawford <cerealean@me.com>
2017-07-10 18:30:57 -0500

    Router and nav bar will now lead to user preferences component

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,6 +1,7 @@
 import { Routes }  from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MemberGuard } from './guards/member.guard';
+import { UserPreferencesComponent } from "app/components/user-preferences/user-preferences.component";
 
 export const appRoutes: Routes = [
   {
@@ -11,5 +12,9 @@ export const appRoutes: Routes = [
   {
     path: "home",
     component: HomeComponent
+  },
+  {
+    path: "user-preferences",
+    component: UserPreferencesComponent
   }
 ];
