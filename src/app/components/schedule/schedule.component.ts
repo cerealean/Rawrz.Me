f47c3b2ab26c6fd7a0516e74cfe1123c7a9e4fea
Michael.Crawford <cerealean@me.com>
2017-07-10 22:42:19 -0500

    Beginnings of rendering the schedule

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DateRange } from "app/models/date-range";
 import * as moment from "moment";
 import { ScheduledUser } from "app/models/scheduled-user";
+import { AuthenticationService } from "app/services/authentication/authentication.service";
 
 @Component({
   templateUrl: './schedule.component.html',
@@ -10,9 +11,9 @@ import { ScheduledUser } from "app/models/scheduled-user";
 export class ScheduleComponent implements OnInit {
   public dateRange: DateRange;
   public daysInRange: Date[];
-  public scheduledUsers: ScheduledUser[];
+  public scheduledUsers: ScheduledUser[] = [];
 
-  constructor() {
+  constructor(private authenticationService:AuthenticationService) {
     let beginDate = moment().startOf("day").toDate();
     let endDate = moment().add(1, "week").endOf("day").toDate();
     this.dateRange = new DateRange(beginDate, endDate);
@@ -21,6 +22,30 @@ export class ScheduleComponent implements OnInit {
   ngOnInit() {
     this.daysInRange = this.dateRange.getRange();
     console.log(this.dateRange.getRange());
+    this.daysInRange.map(day => {
+      this.scheduledUsers.push(
+        {
+          scheduledOn: day,
+          position: "Manager",
+          user: this.authenticationService.getCurrentlyLoggedInUser()
+        },
+        {
+          scheduledOn: day,
+          position: "Server", 
+          user: this.authenticationService.getCurrentlyLoggedInUser()
+        },
+        {
+          scheduledOn: day,
+          position: "Server", 
+          user: this.authenticationService.getCurrentlyLoggedInUser()
+        },
+        {
+          scheduledOn: day,
+          position: "Host", 
+          user: this.authenticationService.getCurrentlyLoggedInUser()
+        },
+      );
+    });
   }
 
 }
