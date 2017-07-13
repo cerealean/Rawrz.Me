5e40dee90b7bba05a70d3c44f2820bd1f7945dd8
Michael Crawford <cerealean@me.com>
2017-07-12 23:17:48 -0500

    Mostly editor-suggested changes

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { User } from '../../models/user';
-import { Http } from "@angular/http";
-import { environment } from "environments/environment";
-import { Login } from "app/models/login";
-import { Observable } from "rxjs/Rx";
-import "rxjs/add/operator/map";
+import { Http } from '@angular/http';
+import { environment } from 'environments/environment';
+import { Login } from 'app/models/login';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class LoginService {
-  private readonly loginServiceEndpoint: string = environment.serviceEndpoint + "/Login";
-  private readonly logoutServiceEndpoint: string = environment.serviceEndpoint + "/Logout";
+  private readonly loginServiceEndpoint: string = environment.serviceEndpoint + '/Login';
+  private readonly logoutServiceEndpoint: string = environment.serviceEndpoint + '/Logout';
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -36,14 +37,15 @@ export class LoginService {
     }
   }
 
-  logout(){
+  logout():Observable<any>{
     try {
       const user = this.authenticationService.getCurrentlyLoggedInUser();
+      console.log("made request to logout");
 
       const request = this.http
-        .post(this.loginServiceEndpoint, user)
-        .map(request => request.json());
+        .post(this.loginServiceEndpoint, user);
       request.subscribe(() => {
+        console.log("request came back from logging out");
         this.authenticationService.clearCurrentlyLoggedInUser();
       });
 
