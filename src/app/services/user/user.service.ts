1d03e5b14a2133002ff0769808c9dd15a8150b08
Michael Crawford <Michael.Crawford@vu.com>
2017-07-25 14:58:42 -0500

    Added new user and added call to service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
-import { User } from "app/models/user";
 import { environment } from "environments/environment";
+import { NewUser } from "app/models/newUser";
 
 @Injectable()
 export class UserService {
   private readonly userServiceEndpoint: string = environment.serviceEndpoint + "/User";
   constructor(private http: Http) {}
 
-  public createUser(user: User) {
-    return this.http.post(this.userServiceEndpoint, user);
+  public createUser(newUser: NewUser) : Promise<any> {
+    return this.http
+      .post(this.userServiceEndpoint, newUser)
+      .map(response => response.json())
+      .toPromise();
   }
 }
