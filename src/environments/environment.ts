23d1baffdd562677f699c61b0744ac85df8d06a4
Michael Crawford <cerealean@me.com>
2017-09-16 23:11:56 -0500

    autofix string quotes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  applicationName: "Rawrz",
-  serviceEndpoint: "http://api.michaelcrawford.ninja/api",
-  storageHash: "r@wr13579!#",
-  storageIV: "Th0r!s@m@z1ngz",
-  currentUserKey: "currentUser"
+  applicationName: 'Rawrz',
+  serviceEndpoint: 'http://api.michaelcrawford.ninja/api',
+  storageHash: 'r@wr13579!#',
+  storageIV: 'Th0r!s@m@z1ngz',
+  currentUserKey: 'currentUser'
 };
